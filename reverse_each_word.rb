1fca2dc9c5f59663ec5aef7167976df32c63036d
Mark Stone <markwstone85@gmail.com>
2019-09-19 19:49:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
-require "pry"
+# require "pry"
 
 def reverse_each_word(string)
   array = string.split
   array.collect do |word|
-    binding.pry
+    # binding.pry
      word.reverse 
   end.join(" ")
 end     
